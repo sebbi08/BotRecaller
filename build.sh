81bc1910ad75b04133a73d9ec1f00d9fbd2fefd6
Sebastian Mahr <sebastian.mahr@sovanta.com>
2019-03-02 20:51:28 +0100

    Changed Build Process

--- build.sh
+++ build.sh
@@ -5,6 +5,3 @@ cp -r ** BotRecaller_0.17.2
 rm -rf BotRecaller_0.17.2/BotRecaller_0.17.2
 rm BotRecaller_0.17.2/build.sh
 zip -r BotRecaller_0.17.2.zip BotRecaller_0.17.2
-rm /mnt/d/ModMyFactory_1.9.0_portable/mods/0.17/BotRecaller_0.17.2.zip
-mv BotRecaller_0.17.2.zip /mnt/d/ModMyFactory_1.9.0_portable/mods/0.17
-rm -rf BotRecaller_0.17.2
