eb1c1e07e6565cd43ef75f0eb9264c078231a330
Zevell <zevalgaming@gmail.com>
2020-06-15 20:51:04 +1000

    Fixed setting causing crash
    the code was written wrong due to a misunderstanding of the Factorio API, it has now been fixed and the setting now affects in game entity (robot recaller chest)

--- control.lua
+++ control.lua
@@ -81,7 +81,7 @@ end
 
 
 script.on_event(defines.events.on_tick, function(event)
-	if event.tick % (settings.data.default_value * 60) == 0 then
+	if event.tick % (settings.startup["recall-delay-per-robot"].value * 60) == 0 then
 		checkRecallers()
 	end
 end)
